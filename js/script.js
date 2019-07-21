cad466efe076c763edb6524a1569c492b53d5ade
Carlos Eduardo <40584880+carlosedu13@users.noreply.github.com>
2019-07-21 16:14:25 -0300

    ajax update send function

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
 const mensagemSucesso = document.querySelector('[mensagemSucesso]')
 const form = document.forms[0]
 
-form.onsubmit = function(e) {
+form.onsubmit = async e => {
   e.preventDefault()
-
-  mensagemSucesso.innerText = 'Formulário enviado com sucesso!! Agora é só esperar o retorno'
 
   const form = e.target
   const formData = new FormData(form)
   const options = {
-    method: form.method,
+    method: 'POST',
     body: new URLSearchParams(formData)
   }
 
   try {
-    const res = await fetch(form.action, options)
+    await fetch(form.action, options)
+    mensagemSucesso.innerText = 'Formulário enviado com sucesso!! Agora é só esperar o retorno'
   } catch (err) {
     console.log(err)
   }
 
-  return false
+  return  false
 }
