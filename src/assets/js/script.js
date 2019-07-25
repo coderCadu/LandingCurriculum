b6db062cfe8ed8ec8e1f7939a5682460f93d5ad5
Carlos Eduardo <40584880+carlosedu13@users.noreply.github.com>
2019-07-25 18:10:40 -0300

    ajax modified 2

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -20,28 +20,32 @@ form.onsubmit = async (e) => {
   return false;
 };
 
+// async function enviarDados(formData) {
+//   fetch('https://submit-form.com/agYqUrcY_08g8cTowKwVH', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       Accept: 'application/json',
+//     },
+//     body: JSON.stringify(formData),
+//   })
+//     .then((response) => {
+//       console.log(response);
+//       mensagemSucesso.innerText = 'Formulário enviado com sucesso!! Agora é só esperar o retorno';
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+//   return false;
+// }
+
 // form.addEventListener('submit', (e) => {
 //   e.preventDefault();
 
 //   const formDom = e.target;
 //   const formData = new FormData(formDom);
-//   const options = {
-//     method: 'POST',
-//     headers: {
-//       'content-type': 'application/json',
-//     },
-//     body: JSON.stringify(formData),
-//   };
+//   console.log(new URLSearchParams(formData))
 
-//   async function enviarDados() {
-//     try {
-//       await fetch(formDom.action, options);
-//       mensagemSucesso.innerText = 'Formulário enviado com sucesso!! Agora é só esperar o retorno';
-//     } catch (err) {
-//       return false;
-//     }
-//     return false;
-//   }
-
-//   enviarDados();
+  
+//   // enviarDados(formData);
 // });
