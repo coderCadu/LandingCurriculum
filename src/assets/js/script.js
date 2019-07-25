baf0cb7c3b7d47c50912741df926df7648b4af7c
Carlos Eduardo <40584880+carlosedu13@users.noreply.github.com>
2019-07-25 17:43:40 -0300

    ajax modified

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -19,3 +19,29 @@ form.onsubmit = async (e) => {
   }
   return false;
 };
+
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+
+//   const formDom = e.target;
+//   const formData = new FormData(formDom);
+//   const options = {
+//     method: 'POST',
+//     headers: {
+//       'content-type': 'application/json',
+//     },
+//     body: JSON.stringify(formData),
+//   };
+
+//   async function enviarDados() {
+//     try {
+//       await fetch(formDom.action, options);
+//       mensagemSucesso.innerText = 'Formulário enviado com sucesso!! Agora é só esperar o retorno';
+//     } catch (err) {
+//       return false;
+//     }
+//     return false;
+//   }
+
+//   enviarDados();
+// });
